245209c79b8f80c98e9dcd246f91273b7285d18e
XXAUROR <200103001@stu.sdu.edu.kz>
2022-12-17 19:57:56 +0600

    Added js to another city

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function big(element){
 }
 
 function small(element){
-    element.style.fontSize = "45px";
+    element.style.fontSize = "40px";
 }
 // END HERE BIG SMALL
 
@@ -32,5 +32,25 @@ function change(element){
             document.getElementById("slogan").innerHTML="Almaty, Mametova 47 & Rozybakieva 247a, SDU-Store(Suleyman Demirel University)";
             document.getElementById("number").innerHTML="+7-747-189-56-16 || +7-778-948-98-00";
             break;
+        case "-Astana-":
+            document.getElementById("map").src="./images/astana.png";
+            document.getElementById("slogan").innerHTML="Astana, Uly Dala 7/7 & Qabanbai Batyr 62";
+            document.getElementById("number").innerHTML="+7-777-062-13-09 || +7-747-189-56-16";
+            break;
+        case "-Shymkent-":
+            document.getElementById("map").src="./images/shym.png";
+            document.getElementById("slogan").innerHTML="Shymkent, Tauke khan 43a";
+            document.getElementById("number").innerHTML="+7-747-189-56-16 || +7-778-948-98-00";
+            break;
+        case "-Aqtau-":
+            document.getElementById("map").src="./images/aqtau.png";
+            document.getElementById("slogan").innerHTML="Aqtau, 17 mkr. 95";
+            document.getElementById("number").innerHTML="+7-778-546-07-83 || +7-747-189-56-16";
+            break;    
+        case "-Aqtobe-":
+            document.getElementById("map").src="./images/aqtobe.png";
+            document.getElementById("slogan").innerHTML=" Aqtobe, Mametova 4";
+            document.getElementById("number").innerHTML="+7-777-70-78 || +7-747-189-56-16";
+            break;    
     }
 }
